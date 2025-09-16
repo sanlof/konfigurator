10d7ac112d24b05abbf23486d6b0857698e114d8
Sandra <sandra0924@protonmail.com>
2025-09-16 12:14:26 +0200

    add navbar to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import ProductPage from "./components/ProductPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <ProductPage />
     </>
   );
