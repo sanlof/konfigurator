0a3ef7b332800184451fe5e3857c0832f20a75e6
Sandra <sandra0924@protonmail.com>
2025-09-16 20:59:37 +0200

    connect infobox and configchoices via props

--- src/components/Infobox/Infobox.jsx
+++ src/components/Infobox/Infobox.jsx
@@ -2,11 +2,11 @@ import styles from "./Infobox.module.css";
 import ProductInfo from "./ProductInfo";
 import ConfigChoices from "./ConfigChoices";
 
-export default function Infobox() {
+export default function Infobox({ config, updateConfig }) {
   return (
     <section className={styles.infobox}>
       <ProductInfo />
-      <ConfigChoices />
+      <ConfigChoices config={config} updateConfig={updateConfig} />
     </section>
   );
 }
