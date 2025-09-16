ac912d93e81f45bc93af45b7de803811ea84051a
Sandra <sandra0924@protonmail.com>
2025-09-16 21:01:11 +0200

    add config state and updateconfig function

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,16 +1,32 @@
 import styles from "./ProductPage.module.css";
 import Canvas from "./Canvas/Canvas";
 import Infobox from "./Infobox/Infobox";
+import { useState } from "react";
 
 export default function ProductPage() {
+  const [config, setConfig] = useState({
+    // default lamp settings
+    spooky: false, // set to "off" by default
+    // color: "orange", <- to be implemented
+    // material: "silver", <- to be implemented
+  });
+
+  const updateConfig = (key, value) => {
+    // update lamp settings when user clicks something
+    setConfig((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+  };
+
   return (
     <main>
       <nav className={styles.sitemap}>
         Products / Lamps / <strong>Lamp 02</strong>
       </nav>
       <section className={styles.product}>
-        <Canvas />
-        <Infobox />
+        <Canvas config={config} />
+        <Infobox config={config} updateConfig={updateConfig} />
       </section>
     </main>
   );
