9a558bbf59758d62cc0f97373f5fc0e97f11ae4c
Sandra <sandra0924@protonmail.com>
2025-09-16 20:55:16 +0200

    add props to show if spooky is on or off

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,5 +1,11 @@
 import styles from "./Canvas.module.css";
 
-export default function Canvas() {
-  return <section className={styles.canvas}>Product Image Goes Here</section>;
+export default function Canvas({ config }) {
+  return (
+    <section className={styles.canvas}>
+      Product Image Goes Here
+      <br />
+      {config.spooky ? <p>Spooky ON</p> : <p>Spooky OFF</p>}
+    </section>
+  );
 }
