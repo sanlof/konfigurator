35b4434441841a53203071eb6fe9b46b48176e15
Sandra <sandra0924@protonmail.com>
2025-09-16 20:57:57 +0200

    connect configchoices and ghostbutton via props

--- src/components/Infobox/ConfigChoices.jsx
+++ src/components/Infobox/ConfigChoices.jsx
@@ -1,10 +1,13 @@
 import styles from "./ConfigChoices.module.css";
 import GhostButton from "../Buttons/GhostButton";
 
-export default function ConfigChoices() {
+export default function ConfigChoices({ config, updateConfig }) {
   return (
     <section className={styles.configChoices}>
-      <GhostButton />
+      <GhostButton
+        value={config.spooky}
+        onChange={(val) => updateConfig("spooky", val)}
+      />
     </section>
   );
 }
