7497eaa18a16ea86e66b47712e6b30851b57e97b
PU-MEriksson <malin.eriksson83@icloud.com>
2025-09-16 13:18:54 +0200

    Add html and text to product info component

---
+++ src/components/Infobox/ProductInfo.jsx
@@ -0,0 +1,23 @@
+export default function ProductInfo() {
+  return (
+    <div>
+      <div>
+        <h2>Lamp 02</h2>
+        <p>499 SEK</p>
+      </div>
+      <div>
+        <h3>Description</h3>
+        <p>
+          The Lava Lamp Model 02 brings a modern twist to a classic design.
+          Featuring a sleek aluminum base and cap, this version enhances any
+          room with its vibrant, slow-moving wax formations suspended in a
+          colorful liquid. With improved heating efficiency and a quicker
+          warm-up time, Model 02 provides a mesmerizing glow in under 30
+          minutes. Perfect for desks, bedrooms, or chill-out spaces, it combines
+          retro charm with updated reliability—an eye-catching piece that adds
+          warmth and personality to any setting.
+        </p>
+      </div>
+    </div>
+  );
+}
