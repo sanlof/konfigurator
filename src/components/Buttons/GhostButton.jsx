4a18e04ac311449edccc199fa2104a936da87944
Sandra <sandra0924@protonmail.com>
2025-09-16 20:51:40 +0200

    use value instead of isclicked with usestate
    this is not as intuitive to me, but it was easier to make the other things work when using this

--- src/components/Buttons/GhostButton.jsx
+++ src/components/Buttons/GhostButton.jsx
@@ -1,35 +1,28 @@
 import styles from "./GhostButton.module.css";
 import { useState } from "react";
 
-export default function GhostButton() {
-  const [isClicked, setIsClicked] = useState(false);
+export default function GhostButton({ value, onChange }) {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
-    if (isClicked) {
-      setIsClicked(false);
-    } else {
-      setIsClicked(true);
-    }
+    onChange(!value); // using value instead of isClicked-state, so it takes the value from onChange via props instead
   };
 
   return (
     <section className={styles.Spooky}>
       <h3>Spooky?</h3>
       <button
-        className={`${styles.GhostButton} ${
-          isClicked ? styles.on : styles.off
-        }`}
+        className={`${styles.GhostButton} ${value ? styles.on : styles.off}`}
         onClick={handleClick}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        {isClicked ? (
+        {value ? (
           <img src="/icons/solar_ghost-bold.png" alt="Ghost icon filled" />
         ) : (
           <img src="/icons/solar_ghost-linear.png" alt="Ghost icon outline" />
         )}
-        {isHovered ? (isClicked ? "Off" : "On") : isClicked ? "On" : "Off"}
+        {isHovered ? (value ? "Off" : "On") : value ? "On" : "Off"}
       </button>
       <article className={styles.addPrice}>+150 SEK</article>
     </section>
