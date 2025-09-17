5e743618760cdb450b5e94e3269cdf4b971113b0
PU-MEriksson <malin.eriksson83@icloud.com>
2025-09-17 14:35:47 +0200

    Add styling to configChoices

--- src/components/Infobox/ConfigChoices.jsx
+++ src/components/Infobox/ConfigChoices.jsx
@@ -6,23 +6,23 @@ import MaterialButton from "../Buttons/MaterialButton";
 export default function ConfigChoices({ config, updateConfig }) {
   return (
     <form className={styles.configChoices}>
-      <fieldset className={styles.fieldset}>
+      <fieldset className={`${styles.fieldset} ${styles.colorFieldset}`}>
         <legend className={styles.legend}>Color</legend>
         <Button>Orange</Button>
         <Button>Blue</Button>
       </fieldset>
-      <fieldset className={styles.fieldset}>
+      <fieldset className={styles.spookyFieldset}>
         <legend className={styles.legend}>Spooky?</legend>
         <GhostButton
           value={config.spooky}
           onChange={(val) => updateConfig("spooky", val)}
         />
       </fieldset>
-      <fieldset className={styles.fieldset}>
+      <fieldset className={`${styles.fieldset} ${styles.materialFieldset}`}>
         <legend className={styles.legend}>Material</legend>
         <MaterialButton />
       </fieldset>
-      <fieldset className={styles.fieldset}>
+      <fieldset className={`${styles.fieldset} {}`}>
         <Button variant="cart">Add to cart</Button>
         <Button variant="favorite" />
       </fieldset>
