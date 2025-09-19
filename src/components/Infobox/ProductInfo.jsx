64111f9eeb1894cbc6de141c506d27f131efdad4
PU-MEriksson <malin.eriksson83@icloud.com>
2025-09-19 11:16:15 +0200

    Add dropdown logic on infotext, add chevron icon and styling

--- src/components/Infobox/ProductInfo.jsx
+++ src/components/Infobox/ProductInfo.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import styles from "./ProductInfo.module.css";
 
 export default function ProductInfo({ config }) {
+  const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
+
+  const toggleDescription = () => {
+    setIsDescriptionOpen(!isDescriptionOpen);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.headingBox}>
@@ -8,17 +14,29 @@ export default function ProductInfo({ config }) {
         <p className={styles.price}>{config.spooky ? 1750 : 1600} SEK</p>
       </div>
       <div className={styles.descriptionBox}>
-        <h3 className={styles.subheading}>Description</h3>
-        <p className={styles.description}>
-          The Lava Lamp Model 02 brings a modern twist to a classic design.
-          Featuring a sleek aluminum base and cap, this version enhances any
-          room with its vibrant, slow-moving wax formations suspended in a
-          colorful liquid. With improved heating efficiency and a quicker
-          warm-up time, Model 02 provides a mesmerizing glow in under 30
-          minutes. Perfect for desks, bedrooms, or chill-out spaces, it combines
-          retro charm with updated reliability—an eye-catching piece that adds
-          warmth and personality to any setting.
-        </p>
+        <h3 className={styles.subheading} onClick={toggleDescription}>
+          Description
+          <span
+            className={`${styles.arrow} ${
+              isDescriptionOpen ? styles.arrowOpen : ""
+            }`}
+          >
+            <img src="/icons/chevron-up.svg" alt="Arrow icon" />
+          </span>
+        </h3>
+        {isDescriptionOpen && (
+          <p className={styles.description}>
+            Immerse yourself in the hypnotic beauty of our Lamp 02 lava lamp.
+            This modern interpretation of the classic design features a premium
+            aluminum base with customizable finishes and mesmerizing wax
+            formations that dance through vibrant liquid. With enhanced heating
+            technology, enjoy faster warm-up times and consistent performance.
+            Choose your perfect color combination and add our exclusive "Spooky"
+            mode for an eerie twist that transforms any space into a mysterious
+            sanctuary. Perfect for creating ambient lighting in bedrooms,
+            studios, or lounge areas.
+          </p>
+        )}
       </div>
     </div>
   );
